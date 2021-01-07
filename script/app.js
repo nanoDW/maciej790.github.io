da43d6f4367b72b47d06dcd40779a2f47cc10cab
maciej790 <maciejmw126@gmail.com>
2021-01-07 19:12:52 +0100

    refactoring v2

--- script/app.js
+++ script/app.js
@@ -5,45 +5,43 @@ const firstViewElement = document.querySelector(".firstView");
 const secondViewElement = document.querySelector("section");
 const refreshBtn = document.querySelector("i");
 
-//all weather data
-const weather = [];
-
-const handleClickSearch = (e) => {
+const handleClickSearch = async (e) => {
   e.preventDefault();
 
-  inputValue.value ? getDataApi() : alert("First You must type a value !");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=962e96c7edd5b917153804d1e003d0ff&units=metric`;
+  const weatherData = await getDataFromApi(url);
+  const item = await createWeatherItem(weatherData);
+  handleShow();
+  createWeatherElement(item);
+
+  inputValue.value = "";
 };
 
-const getDataApi = async () => {
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=962e96c7edd5b917153804d1e003d0ff&units=metric`;
-
-  const response = await fetch(URL);
-  const data = await response.json();
-  const status = response.status;
-
-  status != 404 ? createWeatherObj(data) : alert("Ohh, we have error :(");
+//fetch data
+const getDataFromApi = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    alert("Sorry we cannot display data :(");
+  } else {
+    return await response.json();
+  }
 };
 
-const createWeatherObj = (data) => {
-  //descrutcuring api object
-  const { temp, humidity } = data.main;
-
-  const { icon, description } = data.weather[0];
+const createWeatherItem = async (weatherData) => {
+  const { temp, humidity } = weatherData.main;
+  const { description, icon } = weatherData.weather[0];
 
   const item = {
     temp: Math.round(temp),
-    humidity,
-    icon,
-    description,
+    humidity: humidity,
+    description: description,
+    icon: `http://openweathermap.org/img/wn/${icon}.png`,
   };
 
-  weather.push(item);
-
-  showSection();
-  createElement(item);
+  return item;
 };
 
-const createElement = (item) => {
+const createWeatherElement = (item) => {
   const { temp, humidity, description, icon } = item;
 
   const tempEl = document.createElement("p");
@@ -53,22 +51,21 @@ const createElement = (item) => {
 
   tempEl.innerHTML = `${temp}&#176;C`;
   humidityEl.innerHTML = `${humidity}%`;
+  imageEl.src = icon;
   descriptionEl.innerHTML = description;
-
-  const imageURL = `http://openweathermap.org/img/wn/${icon}.png`;
-
-  imageEl.src = imageURL;
 
   secondViewElement.append(tempEl, humidityEl, imageEl, descriptionEl);
 };
 
-const showSection = () => {
+const handleShow = () => {
   firstViewElement.classList.toggle("top");
   secondViewElement.classList.toggle("display");
-  refreshBtn.classList.toggle("show");
+  refreshBtn.classList.toggle("display");
 };
 
-const refreshPage = () => location.reload();
+const handleRefreshPage = () => {
+  location.reload();
+};
 
 searchBtn.addEventListener("click", handleClickSearch);
-refreshBtn.addEventListener("click", refreshPage);
+refreshBtn.addEventListener("click", handleRefreshPage);
